10eb48525eccef3157d6543801d83b05536f48d1
Veronika Kulik <advertisingkiwi@gmail.com>
2020-05-16 16:39:37 +0300

    Added DB initialization

--- server.js
+++ server.js
@@ -1,6 +1,39 @@
 const express = require('express');
 const app = express();
+const fs = require('fs-extra');
 
-app.listen(6666, () => {
-	console.log('server is started');
-});
+const DB_PATH_DIR = __dirname + '/db/';
+const DB_PATH_EMPLOYES_FILE_NAME = 'employes.json';
+const DB_PATH_EMPLOYES = DB_PATH_DIR + DB_PATH_EMPLOYES_FILE_NAME;
+
+dbInit();
+//startServer();
+
+//database
+function dbInit() {
+  //make sure db directory exists
+	fs.ensureDirSync(DB_PATH_DIR, 0o2775);
+
+  //make sure employes.json exists 
+  fs.ensureFileSync(DB_PATH_EMPLOYES);
+
+  //make sure employes.json is valid
+  const content = fs.readFileSync(DB_PATH_EMPLOYES);
+
+  try {
+  	const parsedData = JSON.parse(content);
+	} catch (e) {
+		fs.writeFileSync(DB_PATH_EMPLOYES, '{}');
+	}
+
+}
+
+//start server
+function startServer() {
+	app.listen(6666, () => {
+		console.log('server is started');
+	});
+}
+
+
+//routing
